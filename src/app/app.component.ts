6940531b64ec08130996be5f3d30505327ee877a
Beau Barclay <beauwarnerbarclay@gmail.com>
2024-02-06 11:45:12 -0500

    initial check in

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component} from "@angular/core";
+
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["../styles.less"]
+})
+export class AppComponent {
+  title = 'WITX'
+}
