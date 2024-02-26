d43d75e296fa3f961ab4eb6789cd5c4c659fb262
bbarclay28 <119443927+bbarclay28@users.noreply.github.com>
2024-02-25 21:06:16 -0500

    Added employment status selection, input variable storage, and changed structure of form.

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component} from '@angular/core';
 
 
 @Component({
@@ -6,9 +6,20 @@ import { Component, Input } from '@angular/core';
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.less']
 })
-export class RegistrationComponent {
+export class RegistrationComponent{
 
-  firstName: string = "";
-  lastName: string = "";
-  
+//Declare variables for storage
+firstName: string = '';
+lastName: string = '';
+fullName: string = '';
+
+status: string = '';
+fullTime: boolean = false;
+partTime: boolean = false;
+student: boolean = false;
+//test: string = "";
+
+  onClickRegister(){
+    this.fullName = "Your full name is " + this.firstName + " " + this.lastName
+  }
 }
